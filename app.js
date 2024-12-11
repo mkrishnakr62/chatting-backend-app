c92d39a757c96e95ce6dafdb04814a2a53361ddf
krishna <mkrishnakr62@gmail.com>
2024-12-11 15:43:22 +0530

    removing cors option

--- app.js
+++ app.js
@@ -42,16 +42,12 @@ cloudinary.config({
 
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-     process.env.CLIENT_URL,
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-  // preflightContinue: true,
+  origin: "*", // Allow requests from all origins
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: false, // Disable credentials for "allow all"
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 
 const app =express();
 const server = createServer(app);
